0dbab5f9b1700c3848021dc3c3d65ab797c7aea0
Lorenz Gerber <gerberlo@gis.a-star.edu.sg>
2018-09-28 16:30:56 +0800

    added tags in the launch scripts

--- beacon-ui/amazonAMI.sh
+++ beacon-ui/amazonAMI.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws ec2 run-instances --image-id  ami-a59b49c6 --key gerber --block-device-mapping DeviceName=/dev/xvda,Ebs={VolumeSize=8} --subnet subnet-6c9f8d2a --iam-instance-profile Name=docker-reg --subnet-id subnet-6c9f8d2a --instance-type t2.micro --user-data file://cloudinit.txt
+aws ec2 run-instances --image-id  ami-a59b49c6 --key gerber --block-device-mapping DeviceName=/dev/xvda,Ebs={VolumeSize=8} --subnet subnet-6c9f8d2a --iam-instance-profile Name=docker-reg --subnet-id subnet-6c9f8d2a --instance-type t2.micro --user-data file://cloudinit.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=beacon-ui},{Key=owner,Value=lorenz},{Key=type,Value=beacon-ui}]'
