a0e876c0f98795f7972e1057e32bfbf7c2342195
Lorenz Gerber <gerberlo@gis.a-star.edu.sg>
2018-09-28 15:49:25 +0800

    added launchers

---
+++ pbs_nextflow/launchXenial.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+aws ec2 run-instances --image-id ami-1c6627f6 --key gerber --security-group-ids sg-b8b318c0 --subnet subnet-6c9f8d2a --iam-instance-profile Name=docker-reg --subnet-id subnet-6c9f8d2a --instance-type t2.small --block-device-mapping 'DeviceName=/dev/sda1,Ebs={VolumeSize=20}' --user-data file://cloudinit.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pbs},{Key=owner,Value=lorenz},{Key=type,Value=pbs}]'
